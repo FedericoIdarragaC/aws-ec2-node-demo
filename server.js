e2b15389ad13084351c7e00ed729975ceb0f2ec2
FedericoIdarragaC <federico.idarraga@utp.edu.co>
2022-12-06 10:48:06 -0500

    modified resp

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const PORT = 3000;
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello World!'));
+app.get('/', (req,res) => res.send('<h1>Hello World!</h1>'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
